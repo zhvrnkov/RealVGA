3cf8d14f971117af293e1e739ed70a54e4218d7c
Vlad <vz@Vlads-MacBook-Pro.local>
2023-10-30 02:29:49 +0600

    fill with width and height

--- vga.asm
+++ vga.asm
@@ -8,8 +8,9 @@
 %define stack2(i) ss:[esp + 2 * i]
 %define stack4(i) ss:[esp + 4 * i]
 
-push 30
 push 320
+push 200 ; height
+push 160 ; width
 push 0x0c
 call fill
 
@@ -18,23 +19,33 @@ jmp $
 ; stack[0] = color
 ; stack[1] = width
 ; stack[2] = height
+; stack[3] = bytesPerRow
 fill:
-    mov cx, stack2(1)
-
-    mov ax, stack2(2) ; ax = width
-    mul word stack2(3)
-    push ax ; word 320 * 100
-
-    mov bx, 0
     mov ax, 0x0013
     int 0x10
     push VGA
-    mov es, stack2(0)
-.y_iter:
+    pop es
+
+    mov bx, stack2(2)
+    sub stack2(4), bx
+    mov cx, stack2(1)
+
+    mov ax, 0 ; x
+    mov di, 0 ; y
+    mov bx, 0 ; i
+.iter:
     mov byte [es:bx], cl
     inc bx
-    cmp bx, stack2(1)
-    jb .y_iter
+    inc ax
+    cmp ax, stack2(2)
+    jb .iter
+    inc di
+    cmp di, stack2(3)
+    ja .out
+    add bx, stack2(4)
+    mov ax, 0
+    jmp .iter
+.out:
     ret
 
 times 510 - ($ - $$) db 0
