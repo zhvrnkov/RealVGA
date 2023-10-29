c44a4b2618d0d2a81e5e4a7a59692abdc510b5f3
Vlad <vz@Vlads-MacBook-Pro.local>
2023-10-30 02:37:42 +0600

    fill with offset

--- vga.asm
+++ vga.asm
@@ -8,9 +8,11 @@
 %define stack2(i) ss:[esp + 2 * i]
 %define stack4(i) ss:[esp + 4 * i]
 
+push 100 ; y-offset
+push 50 ; x-offset
 push 320
-push 200 ; height
-push 160 ; width
+push 100 ; height
+push 100 ; width
 push 0x0c
 call fill
 
@@ -20,28 +22,36 @@ jmp $
 ; stack[1] = width
 ; stack[2] = height
 ; stack[3] = bytesPerRow
+; stack[4] = x-offset
+; stack[5] = y-offset
 fill:
     mov ax, 0x0013
     int 0x10
     push VGA
     pop es
 
-    mov bx, stack2(2)
-    sub stack2(4), bx
-    mov cx, stack2(1)
-
+    mov di, stack2(6)
+    mov ax, stack2(4)
+    mul di
+    add ax, stack2(5)
+    mov bx, ax ; i
     mov ax, 0 ; x
     mov di, 0 ; y
-    mov bx, 0 ; i
+
+    mov si, stack2(2)
+    sub stack2(4), si
+    mov cx, stack2(1)
 .iter:
     mov byte [es:bx], cl
     inc bx
     inc ax
     cmp ax, stack2(2)
     jb .iter
+
     inc di
     cmp di, stack2(3)
     ja .out
+
     add bx, stack2(4)
     mov ax, 0
     jmp .iter
